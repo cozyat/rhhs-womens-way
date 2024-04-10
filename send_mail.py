39f32f4bc28ecaf17d4baf4f248cb12e1b108a44
cozyat <cozyat@gmail.com>
2024-04-09 23:33:28 -0500

    Quick comments to read

--- send_mail.py
+++ send_mail.py
@@ -19,21 +19,22 @@ def send_newsletter(subject, html_content, sender_email, recipient_emails):
         print("Newsletter sent successfully to", len(recipient_emails), "recipients")
     except Exception as e:
         print("Failed to send newsletter:", e)
+        # debug if print as e
 
 def read_recipient_emails_from_csv(csv_file):
     recipient_emails = []
     with open(csv_file, 'r') as file:
         reader = csv.reader(file)
         for row in reader:
-            recipient_emails.append(row[2])
+            recipient_emails.append(row[2]) # CSV file must have a 2nd row
     return recipient_emails
 
 def read_html_content_from_file(html_file):
     with open(html_file, 'r') as file:
         html_content = file.read()
-    return html_content
+    return html_content # update html before sending
 
-subject = "RHHS Women's Way - March Issue"
+subject = "RHHS Women's Way - March Issue" # change this title
 html_file = "womens-way-WD/content.html"
 sender_email = "rhhswomensway@gmail.com"
 csv_file = "womens-way-WD/signups.csv"
@@ -42,3 +43,5 @@ html_content = read_html_content_from_file(html_file)
 recipient_emails = read_recipient_emails_from_csv(csv_file)
 
 send_newsletter(subject, html_content, sender_email, recipient_emails)
+
+# review any outdated elements, refactor if needed
